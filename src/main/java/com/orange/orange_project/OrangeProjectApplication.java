f7f9755611cb3701218b600cef4c13c7757cd2b1
orange <orange@orangenoMacBook-Air.local>
2026-03-28 12:34:26 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/orange/orange_project/OrangeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.orange.orange_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orange/orange_project/OrangeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.orange_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
